bc90e98eab930cb46a0faddaa7085ff6e825365e
Rene Rivera <grafikrobot@gmail.com>
2017-08-06 19:25:57 -0500

    Partial step toward building Locale.
    We still fail because of the indirect reference to Thread (through
    Level8Group).

--- conanfile.py
+++ conanfile.py
@@ -3,21 +3,25 @@ from conans import ConanFile, tools, os
 class BoostLocaleConan(ConanFile):
     name = "Boost.Locale"
     version = "1.64.0"
-    generators = "txt" 
+    generators = "txt", "boost"
     settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/boostorg/locale"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "locale"
-    build_requires = "Boost.Build/1.64.0@bincrafters/testing" 
+    build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
     requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
                       "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.Detail/1.64.0@bincrafters/testing", \
                       "Boost.Function/1.64.0@bincrafters/testing", \
                       "Boost.Iterator/1.64.0@bincrafters/testing", \
+                      "Boost.Integer/1.64.0@bincrafters/testing", \
                       "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
                       "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing"
+                      "Boost.System/1.64.0@bincrafters/testing", \
+                      "Boost.Thread/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
+                      "Boost.Unordered/1.64.0@bincrafters/testing"
 
                       #assert1 config0 function5 iterator5 smart_ptr4 static_assert1 type_traits3
 
@@ -25,7 +29,26 @@ class BoostLocaleConan(ConanFile):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
+    def build(self):
+        boost_build = self.deps_cpp_info["Boost.Build"]
+        b2_bin_name = "b2.exe" if self.settings.os == "Windows" else "b2"
+        b2_bin_dir_name = boost_build.bindirs[0]
+        b2_full_path = os.path.join(boost_build.rootpath, b2_bin_dir_name, b2_bin_name)
+
+        toolsets = {
+          'gcc': 'gcc',
+          'Visual Studio': 'msvc',
+          'clang': 'clang',
+          'apple-clang': 'darwin'}
+
+        b2_toolset = toolsets[str(self.settings.compiler)]
+        
+        self.run(b2_full_path + " -j4 -a --hash=yes toolset=" + b2_toolset)
+
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="include", src=include_dir)
+
+    def package_info(self):
+        self.cpp_info.libs = ["boost_%s"%(self.lib_short_name)]
 
