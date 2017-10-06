57b7189591f756200a411fa773e9e795e695da63
Jerry W <jerrywiltse@gmail.com>
2017-10-06 11:52:01 -0400

    add use_icu option

--- conanfile.py
+++ conanfile.py
@@ -7,8 +7,15 @@ class BoostLocaleConan(ConanFile):
     url = "https://github.com/bincrafters/conan-boost-locale"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    requires =  "Boost.Level11Group/1.65.1@bincrafters/testing"
+    options = {"use_icu": [True, False]}
+    default_options = "use_icu=False"
    
+    def requirements(self):
+        self.requires.add("Boost.Level11Group/1.65.1@bincrafters/testing")
+
+    def configure(self):
+        self.options["Boost.Level11Group"].use_icu = self.options.use_icu 
+        
     #This library is part of one or more cyclic dependency groups within Boost.
     
     #All members of cyclic dependency groups must be built under single package per group for Conan.
