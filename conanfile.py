2acae6843c51a5569d0d832f5ead332dea0585a0
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:47 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,9 @@ from conans import ConanFile, tools
 class BoostLocaleConan(ConanFile):
     name = "Boost.Locale"
     version = "1.65.1"
-    requires = "Boost.Level11Group/1.65.1@bincrafters/testing"
+    requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
+        "Boost.Level11Group/1.65.1@bincrafters/testing"
     options = {"use_icu": [True, False]}
     default_options = "use_icu=False"
     lib_short_names = ["locale"]
@@ -16,11 +18,13 @@ class BoostLocaleConan(ConanFile):
 
     # BEGIN
 
-    url = "https://github.com/bincrafters/conan-boost-boost-locale"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
+    url = "https://github.com/bincrafters/conan-boost-locale"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
     @property
     def env(self):
