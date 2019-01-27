cef075819a0c0c3c7542334883e819eb19852349
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:49 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostLocaleConan(base.BoostBaseConan):
     name = "boost_locale"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_locale"
     lib_short_names = ["locale"]
     options = {
